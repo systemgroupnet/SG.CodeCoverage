9fe3690aead60d13914cc3ec939feae9756a7462
Aryan Ebrahimpour <oxaryan@outlook.com>
2020-04-17 13:53:21 +0430

    Implement GetVisistedFiles

--- SG.CodeCoverage/Collection/DataCollector.cs
+++ SG.CodeCoverage/Collection/DataCollector.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
+using Newtonsoft.Json;
+using AssembliesMap = System.Collections.Generic.IEnumerable<SG.CodeCoverage.Map.Assembly>;
+using System.Linq;
 
 namespace SG.CodeCoverage.Collection
 {
@@ -18,7 +22,41 @@ namespace SG.CodeCoverage.Collection
 
         public IEnumerable<string> GetVisitedFiles()
         {
-            throw new NotImplementedException();
+            ValidiatePaths();
+            var rawJson = File.ReadAllText(MapFilePath);
+            var assemblies = JsonConvert.DeserializeObject<AssembliesMap>(rawJson);
+
+            var typeIdToSourceMapper = assemblies.Select(asm => asm.Types)
+                .SelectMany(x => x)
+                .ToDictionary(
+                    t => t.Index,
+                    t => t.Methods.ToDictionary(m => m.Index, m => m.Source));
+
+            var result = new HashSet<string>();
+            using (var fs = new FileStream(HitsFilePath, FileMode.Open))
+            using (var br = new BinaryReader(fs))
+            {
+                var typesCount = br.ReadInt32();
+                for (int typeId = 0;  typeId < typesCount; typeId++)
+                {
+                    var methodsCount = br.ReadInt32();
+                    for (int j = 0; j < methodsCount; j++)
+                    {
+                        var methodId = br.ReadInt32();
+                        var source = typeIdToSourceMapper[typeId][methodId];
+                        if (result.Add(source)) yield return source;
+                    }
+                }
+            }
+        }
+
+        public void ValidiatePaths()
+        {
+            if (!File.Exists(MapFilePath))
+                throw new FileNotFoundException("Could not find the map file.");
+
+            if (!File.Exists(HitsFilePath))
+                throw new FileNotFoundException("Could not find the hits file.");
         }
 
     }
