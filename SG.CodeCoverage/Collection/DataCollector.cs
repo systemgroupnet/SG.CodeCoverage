d11944ea901206d52863e2a4dc6a5a8ca84060de
Aryan Ebrahimpour <oxaryan@outlook.com>
2020-04-17 12:26:52 +0430

    Create initial data collector class

---
+++ SG.CodeCoverage/Collection/DataCollector.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SG.CodeCoverage.Collection
+{
+    public class DataCollector
+    {
+        public string MapFilePath { get; }
+
+        public string HitsFilePath { get; }
+
+        public DataCollector(string mapFilePath, string hitsFilePath)
+        {
+            MapFilePath = mapFilePath;
+            HitsFilePath = hitsFilePath;
+        }
+
+        public IEnumerable<string> GetVisitedFiles()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
