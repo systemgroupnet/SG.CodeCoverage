9a30160b80adc9efb92690b6a1007f177a96f3da
Mohammad Dehghan <md.unicorn@gmail.com>
2020-04-18 04:45:22 +0430

    Add a netfx test project

---
+++ SG.CodeCoverage.Tests.NetFx/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SG.CodeCoverage.Tests.NetFx")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SG.CodeCoverage.Tests.NetFx")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("21fc4418-8a3d-45ca-9f48-9345d194328d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SG.CodeCoverage.Tests.NetFx/TestInstrumenter.cs
@@ -0,0 +1,58 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SG.CodeCoverage.Common;
+using SG.CodeCoverage.Instrumentation;
+using System;
+using System.IO;
+
+namespace SG.CodeCoverage.Tests
+{
+    [TestClass]
+    public class TestInstrumenter
+    {
+        public const int PortNumber = 61238;
+
+        private string InstrumentSampleProject()
+        {
+            var tempPath = Path.Combine(Path.GetTempPath(), "SG.CodeCoverage");
+            if (Directory.Exists(tempPath))
+                CleanDirectory(tempPath);
+            else
+                Directory.CreateDirectory(tempPath);
+
+            var oridgAssemblyFileName = typeof(SampleProjectForTest.PrimeCalculator).Assembly.Location;
+            var origPdbFileName = Path.ChangeExtension(oridgAssemblyFileName, "pdb");
+
+            var assemblyFileName = Path.Combine(tempPath, Path.GetFileName(oridgAssemblyFileName));
+            var mapFileName = Path.Combine(tempPath, "map.json");
+
+            File.Copy(oridgAssemblyFileName, assemblyFileName);
+            File.Copy(origPdbFileName, Path.Combine(tempPath, Path.GetFileName(origPdbFileName)));
+
+            Instrumenter instrumenter = new Instrumenter(new[] { assemblyFileName },
+                tempPath, mapFileName, PortNumber, new ConsoleLogger());
+
+            instrumenter.Instrument();
+            return assemblyFileName;
+        }
+
+        private void CleanDirectory(string dirPath)
+        {
+            foreach (var file in Directory.EnumerateFiles(dirPath))
+                File.Delete(file);
+            foreach (var dir in Directory.EnumerateDirectories(dirPath))
+                Directory.Delete(dir, true);
+        }
+
+        [TestMethod]
+        public void TestSampleProjectInstrumented()
+        {
+            var asmFileName = InstrumentSampleProject();
+            CheckIsInstrumented(asmFileName);
+        }
+
+        private void CheckIsInstrumented(string asmFilePath)
+        {
+            // to do
+        }
+    }
+}
