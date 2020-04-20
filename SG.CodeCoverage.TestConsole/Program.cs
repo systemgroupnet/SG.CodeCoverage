508f2efeee5fa8fb4ec666166bda4466a241c22b
Mohammad Dehghan <md.unicorn@gmail.com>
2020-04-20 07:16:59 +0430

    Add TestConsole project, to simplify debugging instrumented assembly using dnSpy

---
+++ SG.CodeCoverage.TestConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using SG.CodeCoverage.Tests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SG.CodeCoverage.TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new TestInstrumenter().TestSampleProjectInstrumented();
+        }
+    }
+}
