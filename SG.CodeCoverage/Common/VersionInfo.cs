e3a136fe80394e5b295a9b9be1b2e337b144c33a
Mohammad Dehghan <md.unicorn@gmail.com>
2020-05-01 08:22:44 +0430

    Update version to 0.11.0

--- SG.CodeCoverage/Common/VersionInfo.cs
+++ SG.CodeCoverage/Common/VersionInfo.cs
@@ -10,7 +10,7 @@ namespace SG.CodeCoverage.Common
 {
     public class VersionInfo : IComparable<VersionInfo>
     {
-        public const string CurrentVersionString = "0.10.0";
+        public const string CurrentVersionString = "0.11.0";
         public int Major { get; }
         public int Minor { get; }
         public int Patch { get; }
