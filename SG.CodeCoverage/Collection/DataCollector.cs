b1f4cfa085a6036d5b9495e18d680fa19c255df4
Mohammad Dehghan <md.unicorn@gmail.com>
2020-06-04 17:48:22 +0430

    Add `GetVisitedMethods`
    By copying `GetVisitedFiles`!
    TODO: Fix it later, by merging these method into `CoverageResult` class family

--- SG.CodeCoverage/Collection/DataCollector.cs
+++ SG.CodeCoverage/Collection/DataCollector.cs
@@ -47,6 +47,12 @@ namespace SG.CodeCoverage.Collection
             return GetVisitedFiles(uniqueId, hits);
         }
 
+        public ISet<string> GetVisitedMethods(string hitsFile)
+        {
+            var (uniqueId, hits) = LoadHits(hitsFile);
+            return GetVisitedMethods(uniqueId, hits);
+        }
+
         public ISet<string> GetVisitedFiles(Guid uniqueId, int[][] hits)
         {
             if (uniqueId != _map.UniqueId)
@@ -69,6 +75,34 @@ namespace SG.CodeCoverage.Collection
                     {
                         var source = typeIdToSourceMapper[typeId][methodId];
                         result.Add(source);
+                    }
+                }
+            }
+            return result;
+        }
+
+        private ISet<string> GetVisitedMethods(Guid uniqueId, int[][] hits)
+        {
+            if (uniqueId != _map.UniqueId)
+                throw new Exception($"Hits file's unique id ({uniqueId}) does not match the unique id in the map file ({_map.UniqueId}).");
+
+            var typeMethodsDict = _map.Assemblies.Select(asm => asm.Types)
+                .SelectMany(x => x)
+                .ToDictionary(
+                    t => t.Index,
+                    t => t.Methods.ToDictionary(m => m.Index));
+
+            var result = new HashSet<string>();
+            for (int typeId = 0; typeId < hits.Length; typeId++)
+            {
+                var typeHits = hits[typeId];
+                for (int methodId = 0; methodId < typeHits.Length; methodId++)
+                {
+                    var hitCount = typeHits[methodId];
+                    if (hitCount > 0)
+                    {
+                        var methodName = typeMethodsDict[typeId][methodId].FullName;
+                        result.Add(methodName);
                     }
                 }
             }
