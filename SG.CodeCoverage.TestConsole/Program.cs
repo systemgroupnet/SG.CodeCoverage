28f8ba251dc8982c05692b0f0865bc2c465e9a3b
Aryan <Oxaryan@outlook.com>
2020-06-07 19:51:40 +0430

    Add instrument by path method for console test

--- SG.CodeCoverage.TestConsole/Program.cs
+++ SG.CodeCoverage.TestConsole/Program.cs
@@ -1,6 +1,8 @@
-﻿using SG.CodeCoverage.Tests;
+﻿using SG.CodeCoverage.Instrumentation;
+using SG.CodeCoverage.Tests;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +18,21 @@ namespace SG.CodeCoverage.TestConsole
             tester.RunSomeCode();
             var visitedFiles = tester.GetVisitedFiles();
             Console.WriteLine(visitedFiles.Count);
+        }
+
+        static void InstrumentPath(string dir, string backupFolder, string dllPattern)
+        {
+            var mapFilePath = Path.Combine(dir, "map.json");
+            var filesToInstrument = Directory
+                    .GetFiles(dir, dllPattern)
+                    .ToList();
+            var instrumenter = new Instrumenter(filesToInstrument, new[] { dir }, dir, mapFilePath, 12398, null)
+            {
+                BackupFolder = backupFolder,
+                RecorderLogFilePath = Path.Combine(dir, "CodeCoverageRecorderLog.txt")
+            };
+            Directory.CreateDirectory(instrumenter.BackupFolder);
+            var map = instrumenter.Instrument();
         }
     }
 }
