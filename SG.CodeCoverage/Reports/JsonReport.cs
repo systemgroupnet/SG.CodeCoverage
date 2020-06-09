6f0a95ae8f124a6b5ac910edcb12bad74f87e1bf
Aryan Ebrahimpour <oxaryan@outlook.com>
2020-06-09 15:00:01 +0430

    Add json report

---
+++ SG.CodeCoverage/Reports/JsonReport.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using SG.CodeCoverage.Coverage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SG.CodeCoverage.Reports
+{
+    public class JsonReport : IReport
+    {
+        public string Format => "json";
+
+        public string Extension => "json";
+
+        public string GenerateReport(CoverageResult coverageResult)
+        {
+            var settings = new JsonSerializerSettings()
+            {
+                NullValueHandling = NullValueHandling.Ignore,
+                Formatting = Formatting.None
+            };
+
+            return JsonConvert.SerializeObject(coverageResult, settings);
+        }
+    }
+}
