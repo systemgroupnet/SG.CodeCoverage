63886f6183443ec06631d71ab3f3e4eb43942ced
Mohammad Dehghan <MohammadHD@systemgroup.net>
2020-12-27 20:04:07 +0330

    Fix CA suppressions

--- SG.CodeCoverage.Recorder/GlobalSuppressions.cs
+++ SG.CodeCoverage.Recorder/GlobalSuppressions.cs
@@ -4,6 +4,8 @@
 // a specific target and scoped to a namespace, type, member, etc.
 
 
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "The exceptions should be sent back to the client that invoked the command.", Scope = "member", Target = "~M:SG.CodeCoverage.Recorder.Server.ProcessCommand(System.String)~System.String")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "This method is async and is not awaited anywhere, so it should not raise any exceptions.", Scope = "member", Target = "~M:SG.CodeCoverage.Recorder.Server.StartAsync(System.Int32)~System.Threading.Tasks.Task")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "This method is async and is not awaited anywhere, so it should not raise any exceptions.", Scope = "member", Target = "~M:SG.CodeCoverage.Recorder.Server.AcceptAsync(System.Net.Sockets.TcpClient)~System.Threading.Tasks.Task")]
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "The exceptions should be sent back to the client that invoked the command.", Scope = "member", Target = "~M:SG.CodeCoverage.Recorder.RecordingController.Server.ProcessCommand(System.String)~System.String")]
+[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "This method is async and is not awaited anywhere, so it should not raise any exceptions.", Scope = "member", Target = "~M:SG.CodeCoverage.Recorder.RecordingController.Server.StartAsync(System.Int32)~System.Threading.Tasks.Task")]
+[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "This method is async and is not awaited anywhere, so it should not raise any exceptions.", Scope = "member", Target = "~M:SG.CodeCoverage.Recorder.RecordingController.Server.AcceptAsync(System.Net.Sockets.TcpClient)~System.Threading.Tasks.Task")]
